2640b8c467454d7211a4f11fc119e6a738a0c4c2
anarbaevaaidana2003 <aidana@DESKTOP-64L46II>
2025-02-19 22:33:42 +0600

    added expressApp

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello")
+import express from 'express';
+const expressApp=express();
+expressApp.get('/hello',(req,res)=>{
+    res.send('hi')
+})
+expressApp.listen(3000,()=>{
+    console.info('Listening at http://localhost:3000')
+})

59ec62c91f905e8b56125139229a1dd0399977b1
anarbaevaaidana2003 <aidana@DESKTOP-64L46II>
2025-02-22 21:38:41 +0600

    added layout

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,14 +4,16 @@ import { ViewsIdeaPage } from "./pages/ViewsIdeaPage";
 import { viewRouteParams } from "./lib/routes";
 import { BrowserRouter,Route,Routes} from 'react-router-dom'
 import { getAllIdeasRoute,getViewIdeaRoute } from "./lib/routes";
+import { Layout } from "./components/Layout";
  export const App=()=>{
   return (
     <TrpcProvider>
       <BrowserRouter>
       <Routes>
+        <Route element={<Layout/>}>
         <Route path={getAllIdeasRoute()} element={<AllIdeasPage />}/>
         <Route path={getViewIdeaRoute(viewRouteParams)} element={<ViewsIdeaPage />}/>
-
+        </Route>
       </Routes>
     </BrowserRouter>
     </TrpcProvider>

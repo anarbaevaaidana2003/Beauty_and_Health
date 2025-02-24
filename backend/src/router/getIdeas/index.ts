a0a9b948fc3c3549aa5244368aadee469211f656
anarbaevaaidana2003 <aidana@DESKTOP-64L46II>
2025-02-24 23:47:04 +0600

    added createdAt

--- backend/src/router/getIdeas/index.ts
+++ backend/src/router/getIdeas/index.ts
@@ -9,8 +9,13 @@ export const getIdeasTrpcRoute = trpc.procedure.query(async ({ ctx }) => {
       nick: true,
       name: true,
       description: true,
+      createdAt:true,
+    },
+    orderBy:{
+      createdAt:'desc',
     },
   })
+  console.log('ideas',ideas)
 
   return { ideas }
 })
